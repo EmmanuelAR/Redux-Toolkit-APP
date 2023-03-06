ec9a70032dd2552553c968e8f0a3f7156082f549
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2023-03-06 10:22:35 -0600

    Adding increment-decrement-incrementBy

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
-import { increment } from "./store/slices/counter";
+import { decrement, increment, incrementBy } from "./store/slices/counter";
 
 function App() {
   const { counter } = useSelector((state) => state.counter);
   const dispatch = useDispatch();
+  const incrementNumber = 5;
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Redux Toolkit</h1>
       <div className="card">
+        <h2>count is {counter}</h2>
         <button
           onClick={() => {
             dispatch(increment());
           }}
         >
-          count is {counter}
+          Increment
+        </button>
+        <button
+          onClick={() => {
+            dispatch(decrement());
+          }}
+        >
+          Decrement
+        </button>
+        <button
+          onClick={() => {
+            dispatch(incrementBy(incrementNumber));
+          }}
+        >
+          Increment by {incrementNumber}
         </button>
       </div>
     </div>
