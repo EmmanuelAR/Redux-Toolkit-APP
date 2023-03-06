c3116fa6b676f60c81a5e2df7639833ee0f23bb9
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2023-03-06 11:00:00 -0600

    Adding pokemon page

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,12 @@
+export const PokemonApp = () => {
+  return (
+    <>
+      <h1>PokemonApp</h1>
+      <hr />
+      <ul>
+        <li>Hola</li>
+        <li>Hola</li>
+      </ul>
+    </>
+  );
+};
