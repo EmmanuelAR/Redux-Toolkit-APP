a3721a982244e3c8046f772c2a270b96fdcf932c
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2023-03-10 20:33:33 -0600

    Adding axios to  make http calls

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,3 +1,4 @@
+import { pokemonApi } from "../../../api/pokemonApi";
 import { setPokemons, startLoadingPokemons } from "./PokemonSlice";
 
 //  Funcion que retorna el llamado de otra
@@ -6,13 +7,10 @@ import { setPokemons, startLoadingPokemons } from "./PokemonSlice";
 export const getPokemons = (page = 0) => {
   return async (dispatch, getState) => {
     dispatch(startLoadingPokemons());
-    // hacer llamado http
-    const resp = await fetch(
-      `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`
-    );
 
-    const data = await resp.json();
-    console.log(data);
+    const { data } = await pokemonApi.get(
+      `/pokemon?limit=10&offset=${page * 10}`
+    );
 
     dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
   };
